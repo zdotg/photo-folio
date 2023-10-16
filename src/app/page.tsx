0beb3618653ec5a23eba09a42c922d88e519d24e
Zach Gallman <zach.ecab@gmail.com>
2023-10-16 18:01:12 -0400

    Reformated images, their folders, and names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,25 @@ export default function Home() {
     <main className="flex flex-col min-h-screen max-w-screen-md pl-0 ml-40 ">
       <div className="container flex flex-wrap w-5/12 my-auto">
         <div className="flex flex-row columns-4 px-10 py-5 space-x-3">
-          <Image src="/images/photo-book/01292018-28.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
-          <Image src="/images/photo-book/Brooke-108.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
-          <Image src="/images/photo-book/Brooke-128.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
-          <Image src="/images/photo-book/Brooke-146.jpg" className="hover:scale-125" width={300} height={300} alt="" />
+          <Image src="/images/2018/feb-2018/Feb21-6.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
+          <Image src="/images/2018/feb-2018/Feb21-38.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
+          <Image src="/images/2018/july-2018/july-18_4407.jpg" className="hover:scale-125" width={300} height={300} alt=""/>
+          <Image src="/images/2018/nyc-2018/nyc-18_5637.jpg" className="hover:scale-125" width={300} height={300} alt="" />
           <button>2018</button>
         </div>
         
         <div className="flex flex-row columns-4 px-10 py-5 space-x-3">
-          <Image src="/images/nyc-2019/NY-1.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/nyc-2019/NY-2.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/nyc-2019/Ny-3.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/nyc-2019/NY-4.jpg" className="hover:scale-125" width={300} height={300} alt="" />
+          <Image src="/images/2019/nyc-2019/nyc-19-1.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2019/nyc-2019/nyc-19-2.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2019/photo-book/PB-00003.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2019/photo-book/PB-00009.jpg" className="hover:scale-125" width={300} height={300} alt="" />
           <button>2019</button>
         </div>
         <div className="flex flex-row columns-4 px-10 py-5 space-x-3">
-          <Image src="/images/jan-2020/jan2020-1.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/jan-2020/jan2020-15.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/jan-2020/jan2020-12.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
-          <Image src="/images/jan-2020/jan2020-22.jpg" className="hover:scale-125" width={300} height={300} alt="" />
+          <Image src="/images/2020/april-2020/BKApril3-1.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2020/april-2020/BKApril3-12.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2020/jan-2020/jan2020-1.jpg" className="hover:scale-125"  width={300} height={300} alt=""/>
+          <Image src="/images/2020/jan-2020/jan2020-19.jpg" className="hover:scale-125" width={300} height={300} alt="" />
           <button>2020</button>
         </div>
       </div>
